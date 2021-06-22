fe2d8df93689abaada8aa362cc1bd3e16c8fdcd9
imraanishmail <imraanishmail@gmail.com>
2021-06-22 15:00:49 -0400

    Contact Form Needs To Be Tested

---
+++ from-to-email.php
@@ -0,0 +1,28 @@
+<?php
+  if(!isset($_POST['submit'])) {
+
+    echo "Please submit form.";
+  }
+
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  //Validation
+  if(empty($name) || empty($visitor_email)) {
+     echo "Name and email are mandatory!";
+     exit;
+  }
+
+  $email_from = $visitor_email;
+  $email_subject = "New Form Submission";
+  $email_body = "You have received a message from the user $name.\n" . 
+                "email address: $visitor_email\n" . "Here is the message:\n $message";
+  
+  $to = "knarine12@hotmail.ca";
+  $headers = "From: $email_from \r\n";
+
+  mail($to, $email_subject, $email_body, $headers);
+  //header("Location: contact.html?mailsend");
+  
+?>
